f235caeadf40a47ea6617181f92ec6757184c8b6
unknown <you@example.com>
2019-10-18 12:09:05 -0400

    update JS

--- assets/oregontrail.js
+++ assets/oregontrail.js
@@ -1,48 +1,48 @@
 (function () {
     // console.log("hello");
 
-//A Traveler has a few properties:
+    //A Traveler has a few properties:
 
-//a name (string) that must be provided as a parameter to the constructor
-//an amount of food (number) with an initial value of 1
-//an isHealthy (boolean) to indicate whether they are sick, 
-//with an initial value of true
+    //a name (string) that must be provided as a parameter to the constructor
+    //an amount of food (number) with an initial value of 1
+    //an isHealthy (boolean) to indicate whether they are sick, 
+    //with an initial value of true
 
 
-//A Wagon has a few properties as well:
+    //A Wagon has a few properties as well:
 
-//a capacity (number) that must be provided as a parameter 
-//to the constructor, sets the maximum number of passengers 
-//the wagon can hold
-//a passengers list (array) which is initially empty
+    //a capacity (number) that must be provided as a parameter 
+    //to the constructor, sets the maximum number of passengers 
+    //the wagon can hold
+    //a passengers list (array) which is initially empty
 
-//Operations
-//Write the following prototype methods:
+    //Operations
+    //Write the following prototype methods:
 
-//Traveler.prototype.hunt()
-//Increase the traveler's food by 2.
+    //Traveler.prototype.hunt()
+    //Increase the traveler's food by 2.
 
-//Traveler.prototype.eat()
-//Consumes 1 unit of the traveler's food. If the traveler doesn't 
-//have any food to eat, the traveler is no longer healthy.
+    //Traveler.prototype.eat()
+    //Consumes 1 unit of the traveler's food. If the traveler doesn't 
+    //have any food to eat, the traveler is no longer healthy.
 
-//Wagon.prototype.getAvailableSeatCount()
-//Return the number of empty seats, determined by the capacity 
-//set when the wagon was created, compared to the number of 
-//passengers currently on board.
+    //Wagon.prototype.getAvailableSeatCount()
+    //Return the number of empty seats, determined by the capacity 
+    //set when the wagon was created, compared to the number of 
+    //passengers currently on board.
 
-//Wagon.prototype.join(traveler)
-//Add the traveler to the wagon if there is space. If the wagon 
-//is already at maximum capacity, don't add them.
+    //Wagon.prototype.join(traveler)
+    //Add the traveler to the wagon if there is space. If the wagon 
+    //is already at maximum capacity, don't add them.
 
-//Wagon.prototype.shouldQuarantine()
-//Return true if there is at least one unhealthy person in the wagon. 
-//Return false if not.
+    //Wagon.prototype.shouldQuarantine()
+    //Return true if there is at least one unhealthy person in the wagon. 
+    //Return false if not.
 
-//Wagon.prototype.totalFood()
-//Return the total amount of food among all occupants of the wagon.
+    //Wagon.prototype.totalFood()
+    //Return the total amount of food among all occupants of the wagon.
 
-//......................................................................
+    //......................................................................
 
     function Traveler(name) {
         this.name = name;
@@ -51,27 +51,30 @@
 
     }
 
-    function Wagon(capacity) {
-        this.capacity = capacity;
-        this.passengers = [];
-    }
-
     Traveler.prototype.hunt() = function () {
         this.food = this.food + 2
     }
 
     Traveler.prototype.eat() = function () {
-        this.food = this.food - 1
-        if (this.food = 0)
-            this.isHealthy = false
+        if (this.food >= 1) {
+            this.food = this.food - 1;
+        }
+        else {
+            this.isHealthy = false;
+        }
+    }
+
+    function Wagon(capacity) {
+        this.capacity = capacity;
+        this.passengers = [];
     }
 
     Wagon.prototype.getAvailableSeatCount() = function () {
-        
+        return this.capacity - this.passengers.length;
     }
 
     Wagon.prototype.join(traveler) = function () {
-
+        
     }
 
     Wagon.prototype.shouldQuarantine() = function () {
